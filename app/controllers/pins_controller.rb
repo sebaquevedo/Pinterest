f8f7b29596f2613748b285fbe8a31486e7d21466
sebastian quevedo <sebaqd91@gmail.com>
2017-04-06 14:50:16 -0300

    last modifications

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,11 +1,27 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!,except:[:index, :show]
+  
+
+
+ def pinsof
+    @user_id = params[:user_id]
+    @user = User.find(@user_id)
+    @pins = @user.pins
+  end
+
+  def mypins
+    @pins = current_user.pins 
+  end
+
 
   # GET /pins
   # GET /pins.json
   def index
     @pins = Pin.all
   end
+
+
 
   # GET /pins/1
   # GET /pins/1.json
@@ -25,6 +41,7 @@ class PinsController < ApplicationController
   # POST /pins.json
   def create
     @pin = Pin.new(pin_params)
+    @pin.user_id = current_user.id
 
     respond_to do |format|
       if @pin.save
@@ -69,6 +86,6 @@ class PinsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
-      params.require(:pin).permit(:photo, :description, :name)
+      params.require(:pin).permit(:photo, :description, :name, :user_id)
     end
 end
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,4 +1,5 @@
 class Pin < ApplicationRecord
 validates :photo, presence:true
 validates :description, presence:true
+belongs_to :user
 end
